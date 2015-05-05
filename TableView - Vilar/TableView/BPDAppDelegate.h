772ffd048193e499c5c9975baa5899f81e75f5c0
leonardo fernandes farias <leonardo@MacBook-Pro-de-leonardo.local>
2015-05-05 11:28:05 -0400

    Projeto back up do Vilar

---
+++ TableView - Vilar/TableView/BPDAppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by Vilar da Camara Neto on 15-04-08.
+//  Copyright (c) 2015 Vilar da Camara Neto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface BPDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
